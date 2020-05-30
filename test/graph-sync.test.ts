584f4dcefe0c211bbba52c32c056599993130371
Ahrnee <ahrndee@gmail.com>
2020-05-30 12:54:41 +0100

    Updated and passing tests

--- test/graph-sync.test.ts
+++ test/graph-sync.test.ts
@@ -1,10 +1,11 @@
-import { observable } from 'mobx';
-import { SyncableCollection, GraphSynchronizer } from '../src';
+import { BookDomainModel, LibraryDomainModel, mockWatchedQueryResult } from '.';
+import { GraphSynchronizer } from '../src';
 import { Logger } from '../src/logger';
-import { AuthorDomainModel, mockWatchedQueryResult, LibraryDomainModel, BookDomainModel } from '.';
 import { Book } from './test-types';
 
 const logger = Logger.make('autoSynchronize.test.ts');
+const PERF_TEST_ITERATION_COUNT_MS = 1000;
+const PERF_TEST_MAX_TIME_MS = 500;
 
 // --------------------------------------------------------------
 // TESTS
@@ -13,22 +14,15 @@ const logger = Logger.make('autoSynchronize.test.ts');
 test('auto synchronize updates properties as expected', () => {
   const libraryDomainModel = new LibraryDomainModel();
 
-  expect(libraryDomainModel.name).toBeFalsy();
-  expect(libraryDomainModel.city$).toBeFalsy();
-  expect(libraryDomainModel.authors.size$).toBeFalsy();
-
+  // SETUP
   const graphSynchronizer = new GraphSynchronizer({
     sourcePathMap: [
       {
         path: 'authors.books',
         options: {
           domainObjectCreation: {
-            makeKeyFromSourceElement: (book: Book) => {
-              return book.id;
-            },
-            makeKeyFromDomainItem: (book: BookDomainModel) => {
-              return book.id;
-            },
+            makeKeyFromSourceElement: (book: Book) => book.id,
+            makeKeyFromDomainItem: (book: BookDomainModel) => book.id,
             makeTargetCollectionItemFromSourceItem: (book: Book) => new BookDomainModel(),
           },
         },
@@ -36,16 +30,29 @@ test('auto synchronize updates properties as expected', () => {
     ],
     globalPropertyNameTransformations: { tryStandardPostfix: '$' },
   });
+
+  // POSTURE VERIFICATION
+  expect(libraryDomainModel.name).toBeFalsy();
+  expect(libraryDomainModel.city$).toBeFalsy();
+  expect(libraryDomainModel.authors.size$).toBeFalsy();
+
+  // EXECUTE
   graphSynchronizer.synchronize({ rootDomainObject: libraryDomainModel, rootsourceObject: mockWatchedQueryResult.library });
 
+  // RESULTS VERIFICATION
   expect(libraryDomainModel.name).not.toBeFalsy();
   expect(libraryDomainModel.city$).not.toBeFalsy();
   expect(libraryDomainModel.authors.size$).not.toBeFalsy();
 
   expect(libraryDomainModel.name).toEqual(mockWatchedQueryResult.library.name);
   expect(libraryDomainModel.city$).toEqual(mockWatchedQueryResult.library.city);
-  expect(libraryDomainModel.authors.size$).toEqual(3);
 
+  expect(libraryDomainModel.authors.size$).toEqual(mockWatchedQueryResult.library.authors.length);
+  expect(libraryDomainModel.authors.array$[0].id).toEqual(mockWatchedQueryResult.library.authors[0].id);
+  expect(libraryDomainModel.authors.array$[0].name$).toEqual(mockWatchedQueryResult.library.authors[0].name);
+  expect(libraryDomainModel.authors.array$[0].age$).toEqual(mockWatchedQueryResult.library.authors[0].age);
+
+  expect(libraryDomainModel.authors.array$[0].books.length).toEqual(mockWatchedQueryResult.library.authors[0].books.length);
   expect(libraryDomainModel.authors.array$[0].books[0].id).toEqual(mockWatchedQueryResult.library.authors[0].books[0].id);
   expect(libraryDomainModel.authors.array$[0].books[0].title$).toEqual(mockWatchedQueryResult.library.authors[0].books[0].title);
   expect(libraryDomainModel.authors.array$[0].books[0].publisher).toBeDefined();
@@ -56,22 +63,42 @@ test('auto synchronize updates properties as expected', () => {
 //
 //
 //
-test.skip('performance', () => {
-  // Setup
-  const iterations = 10000;
-  const authorDomainModel = new AuthorDomainModel();
-  const graphSynchronizer = new GraphSynchronizer({ globalPropertyNameTransformations: { tryStandardPostfix: '$' } });
+test('performance', () => {
+  // SETUP
+  const iterations = PERF_TEST_ITERATION_COUNT_MS;
+  const libraryDomainModel = new LibraryDomainModel();
 
+  const graphSynchronizer = new GraphSynchronizer({
+    sourcePathMap: [
+      {
+        path: 'authors.books',
+        options: {
+          domainObjectCreation: {
+            makeKeyFromSourceElement: (book: Book) => book.id,
+            makeKeyFromDomainItem: (book: BookDomainModel) => book.id,
+            makeTargetCollectionItemFromSourceItem: (book: Book) => new BookDomainModel(),
+          },
+        },
+      },
+    ],
+    globalPropertyNameTransformations: { tryStandardPostfix: '$' },
+  });
+
+  // EXECUTE
   const startTime = performance.now();
 
-  // Execute
   for (let i = 0; i < iterations; i++) {
-    graphSynchronizer.synchronize({ rootDomainObject: authorDomainModel, rootsourceObject: mockWatchedQueryResult.library.authors[0] });
+    graphSynchronizer.synchronize({ rootDomainObject: libraryDomainModel, rootsourceObject: mockWatchedQueryResult.library });
   }
 
-  // Assess
   const finishTime = performance.now();
   const totalTimeMs = Math.round(finishTime - startTime);
 
-  logger.info(`${iterations} graphSynchronizer.synchronize iterations: totalTime: ${totalTimeMs} ms (${totalTimeMs / 1000}s) = per iteration ${totalTimeMs / iterations}ms (${totalTimeMs / iterations / 1000}) `);
+  // VERIFY
+  logger.info(
+    `${iterations} graphSynchronizer.synchronize iterations: totalTime: ${totalTimeMs} milliseconds (${totalTimeMs / 1000} seconds) = per iteration ${totalTimeMs / iterations} milliseconds (${
+      totalTimeMs / iterations / 1000
+    } seconds) `,
+  );
+  expect(totalTimeMs).toBeLessThan(PERF_TEST_MAX_TIME_MS);
 });
